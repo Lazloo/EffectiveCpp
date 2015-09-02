ec92f3e143ddaf156d4fec22cb8abb3857d633a3
unknown <freier@IBT283.ibt.kfa-juelich.de>
2015-09-02 14:54:31 +0200

    20150902
    - Modify Item 9 and Item 13
    
    Signed-off-by: unknown <freier@IBT283.ibt.kfa-juelich.de>

--- EffectiveCPP_Item13/Project6/main.cpp
+++ EffectiveCPP_Item13/Project6/main.cpp
@@ -2,7 +2,7 @@
 #define GLM_FORCE_RADIANS
 ////Memory Leak
 #include <stdlib.h>
-#include "vld.h"
+//#include "vld.h"
 //#define _DEBUG
 
 #include <fstream>
@@ -23,7 +23,7 @@ public:
 	};
 
 	void testFunction2(void){
-		// Auto Pointer: RAII - Object which calls destructor an avoid memory leak
+		// Auto Pointer: RAII - Object which calls destructor and avoids memory leak
 		std::cout<<"Function 2"<<std::endl;
 		std::auto_ptr<int> ptr1(new int);
 		std::auto_ptr<int> ptr2(new int);
@@ -39,7 +39,7 @@ public:
 	};
 
 	void testFunction3(void){
-		// Shared Pointer: RCSP - Object which calls destructor an avoid memory leak
+		// Shared Pointer: RCSP - Object which calls destructor and avoid memory leak
 		std::cout<<"Function 3"<<std::endl;
 		std::shared_ptr<int> ptr1(new int);
 		std::shared_ptr<int> ptr2(new int);
@@ -48,6 +48,9 @@ public:
 		std::cout<<"ptr1: "<<(*ptr1)<<std::endl;
 		std::cout<<"ptr2: "<<*ptr2<<std::endl;
 		// Copy Auto pointer ptr2 get Null pointer
+		// An  RCSP  is  a  smart  pointer  that  keeps  track  of  how  many  objects
+		// point to a particular resource and automatically deletes the resource
+		// when nobody is pointing to it any longer.
 		ptr1 = ptr2;
 		std::cout<<"ptr1: "<<*ptr1<<std::endl;
 		// Points to nothing (NUll Pointer)
@@ -57,7 +60,10 @@ public:
 	void testFunction4(void){
 		std::cout<<"Function 4"<<std::endl;
 		// NO ARRAY ALLOCATIONS!!!!
+		// That’s  because  vector  and  string  can  almost  always  replace
+		// dynamically allocated arrays.
 		std::shared_ptr<int> ptr1(new int[10]);
+
 		std::cout<<"NO ARRAY ALLOCATIONS!!!!"<<std::endl;
 	};
 private:
--- EffectiveCPP_Item9/Project6/main.cpp
+++ EffectiveCPP_Item9/Project6/main.cpp
@@ -11,6 +11,8 @@
 #include <vector>
 #include <stdio.h>
 
+
+// ##################### Derive Abstract Class
 class testBaseClass{
 public:
 	//# Does not work since virtual function is not yet defined
@@ -25,6 +27,18 @@ private:
 	virtual void virtualFct(void) = 0;
 };
 
+class testDerivedClass :private testBaseClass{
+public:
+	testDerivedClass(){ virtualFct(); };
+	~testDerivedClass(){};
+
+private:
+	//virtual void virtualFct(){};
+	virtual void virtualFct(){ std::cout << "Call virtual function" << std::endl; };
+};
+// #####################
+
+// ##################### Multiple Inheritance fom interfaces
 class testInterface{
 public:
 	testInterface(){};
@@ -43,7 +57,7 @@ private:
 	virtual void virtualFct2(void) = 0;
 };
 
-class testDerivedFromInterface :private testInterface&private testInterface2{
+class testDerivedFromInterface :private testInterface,private testInterface2{
 public:
 	testDerivedFromInterface(){};
 	~testDerivedFromInterface(){};
@@ -52,17 +66,9 @@ private:
 	virtual void virtualFct1(void){ std::cout << "virtualFct1" << std::endl; };
 	virtual void virtualFct2(void){ std::cout << "virtualFct2" << std::endl; };
 };
+// #####################
 
-class testDerivedClass:private testBaseClass{
-public: 
-	testDerivedClass(){virtualFct();};
-	~testDerivedClass(){};
-	
-private:
-	//virtual void virtualFct(){};
-	virtual void virtualFct(){std::cout<<"Call virual function"<<std::endl;};
-};
-
+// ##################### Base Class needs an input for the constructor
 class testBaseClass2{
 public: 
 	testBaseClass2(int inputInt){virtualFctReplacement(inputInt);};
@@ -84,16 +90,18 @@ public:
 	
 private:
 };
+// ##################### 
 
-// ##############################
+// ############################## Multiple Inheritance fom interfaces, each constructor does something
 class interfaceClass1
 {
 public:
 	interfaceClass1(){ std::cout << "Interface 1" << std::endl; };
 	~interfaceClass1(){};
 
-private:
 	virtual void virtualFct1(void) = 0;
+private:
+	
 };
 
 class interfaceClass2
@@ -102,8 +110,9 @@ public:
 	interfaceClass2(){ std::cout << "Interface 2" << std::endl; };	
 	~interfaceClass2(){};
 
-private:
 	virtual void virtualFct2(void) = 0;
+private:
+	
 };
 
 class abstractCalss
@@ -112,8 +121,9 @@ public:
 	abstractCalss(){ std::cout << "Interface 2" << std::endl; };	
 	~abstractCalss(){};
 
-private:
 	virtual void virtualFct3(void) = 0;
+private:
+	
 	void realFct3(void){ std::cout << "Real Function"<<std::endl; };
 };
 
@@ -125,9 +135,10 @@ public:
 	classDerivedFromInterfaces(){ std::cout << "Class Multiple Inherince" << std::endl; };
 	~classDerivedFromInterfaces(){};
 
+	virtual void virtualFct1(void){ std::cout << "virtualFct_1" << std::endl; };
+	virtual void virtualFct2(void){ std::cout << "virtualFct_2" << std::endl; };
 private:
-	virtual void virtualFct1(void){ std::cout << "virtualFct1" << std::endl; };
-	virtual void virtualFct2(void){ std::cout << "virtualFct2" << std::endl; };
+	
 };
 // ##############################
 
@@ -135,12 +146,16 @@ int main(void){
 	// testDerivedClass is dervied from an abract function (contains one virtual function)
 	// Virtual function is completed and called in the constructor of testDerivedClass
 	testDerivedClass testObject;
-	// Standard derived class. No virtual function exist in basis class
+	// Standard derived class. No virtual function exist in basis class.
+	// Base Class needs an input for the constructor
 	testDerivedClass2 testObject2(13);
 
 	std::cout << std::endl;
 	std::cout << "----Multiple Inherince----" << std::endl;
 	classDerivedFromInterfaces testObjectMultipleinherince;
+	testObjectMultipleinherince.virtualFct1();
+	testObjectMultipleinherince.virtualFct2();
+
 	system("Pause");
 	return 0;
 }
